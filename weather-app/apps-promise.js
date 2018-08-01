4768bd7d5da5fd24c9ad8df3cf2664d0d9557250
Guillaume COLOMBIER <gcolombier@skyrepublic.com>
2018-07-31 17:10:15 -0700

    changes made to apps-promise

--- weather-app/apps-promise.js
+++ weather-app/apps-promise.js
@@ -19,9 +19,26 @@ const argv = yargs
 
 var encodedAddress = encodeURIComponent(argv.address);
 var geoCodeUrl=`http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+// var geoCodeUrl=`http://maps.googleapis.com/maps/api/gcode/json?address=${encodedAddress}`;
 
 axios.get(geoCodeUrl).then( (response) => {
-  console.log(response.data);
-}).catch( (e) => {
-  console.log(e);
+  if (response.data.status ==='ZERO_RESULTS' ) {
+    throw new Error('Unable to find that address');
+  }
+  var latitude = response.data.results[0].geometry.location.lat;
+  var longitude = response.data.results[0].geometry.location.lng;
+  var weatherUrl = `https://api.darksky.net/forecast/4b38bc41506beb66ce325bdc2cb3a096/${latitude},${longitude}`
+  console.log(response.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+}).then( (response) => {
+  var temperature = response.data.currently.temperature;
+  var apparentTemperature = response.data.currently.apparentTemperature;
+  console.log(`It's currentl ${temperature}, it feels like ${apparentTemperature}`);
+}).catch( (error) => {
+  console.log(error);
+  if (error.code === 'ENOTFOUND') {
+    console.log('Unable to connect to API servers.');
+  } else {
+    console.log(error.message);
+  }
 });
